8714e9201b70f67743509ecec56b98690e26770a
Viktor Kolomiiets <vitia.kolomiiets@gmail.com>
2022-06-18 11:05:04 +0300

    feat: add anim to nav links on click

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -4,6 +4,12 @@ import IconCalendar28 from "./icons/IconCalendar28.vue";
 import { RouterLink } from "vue-router";
 import IconSettings28 from "./icons/IconSettings28.vue";
 
+
+function toggleAnimClass(e: Event) {
+  const el = (e.target as HTMLAnchorElement);
+  setTimeout(() => { el.classList.add('router-ling-svg-anim') }, 50);
+  setTimeout(() => { el.classList.remove('router-ling-svg-anim') }, 551);
+}
 </script>
 
 <template>
@@ -11,22 +17,25 @@ import IconSettings28 from "./icons/IconSettings28.vue";
     <nav class="h-[49px] max-w-3xl mx-auto grid grid-cols-3 align-middle text-center system-gray-color font-medium">
       <RouterLink to="/"
         class="w-full caption-3 justify-center items-center pt-[5px]"
-        activeClass="system-blue-color">
-        <IconHeart28 class="mx-auto mb-[2px]" />
+        activeClass="system-blue-color"
+        @click="toggleAnimClass">
+        <IconHeart28 class="mx-auto mb-[2px] pointer-events-none" />
         {{ $t('Pages.Summary') }}
       </RouterLink>
 
       <RouterLink to="/calendar"
         class="w-full caption-3 justify-center items-center pt-[5px]"
-        activeClass="system-blue-color">
-        <IconCalendar28 class="mx-auto mb-[2px]" />
+        activeClass="system-blue-color"
+        @click="toggleAnimClass">
+        <IconCalendar28 class="mx-auto mb-[2px] pointer-events-none" />
         {{ $t('Pages.Calendar') }}
       </RouterLink>
 
       <RouterLink to="/settings"
         class="w-full caption-3 justify-center items-center pt-[5px]"
-        activeClass="system-blue-color">
-        <IconSettings28 class="mx-auto mb-[2px]" />
+        activeClass="system-blue-color"
+        @click="toggleAnimClass">
+        <IconSettings28 class="mx-auto mb-[2px] pointer-events-none" />
         {{ $t('Pages.Settings') }}
       </RouterLink>
     </nav>
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import { i18n } from './i18n';
+//import { MotionPlugin } from '@vueuse/motion';
 
 const app = createApp(App);
 app.use(i18n);
 app.use(createPinia());
+//app.use(MotionPlugin);
 app.use(router);
 
 app.mount('#app');
